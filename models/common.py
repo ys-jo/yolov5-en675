dabe4223c183af0e92428bc71f449c65a27449b5
ys-jo <cchhoo407@naver.com>
2024-08-12 08:30:16 +0900

    update align 32

--- models/common.py
+++ models/common.py
@@ -172,10 +172,14 @@ class C3(nn.Module):
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
+        if c_%32!=0:
+            c_ += (c_%32)
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
         self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+        
+        assert c_%32==0, "Concat must be aligned by 32"
 
     def forward(self, x):
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
